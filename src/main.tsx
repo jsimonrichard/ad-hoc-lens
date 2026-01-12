e32b00aaa047e626659a3a9ea570ed2baba33313
J. Simon Richard <jsimonrichard@gmail.com>
2026-01-12 17:51:23 -0500

    Disable the loudest posthog features

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { PostHogProvider } from "posthog-js/react"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { PostHogProvider } from "posthog-js/react";
 
-import "./index.css"
-import App from "./App.tsx"
+import "./index.css";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,12 +11,14 @@ createRoot(document.getElementById("root")!).render(
       apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
       options={{
         api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-        defaults: '2025-05-24',
+        defaults: "2025-05-24",
         capture_exceptions: true,
         debug: import.meta.env.MODE === "development",
+        disable_session_recording: true,
+        enable_heatmaps: false,
       }}
     >
       <App />
     </PostHogProvider>
   </StrictMode>
-)
+);
